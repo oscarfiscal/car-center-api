dfa0a63d569bcb686c1f58459c1cedb34cb09357
oscarfiscal <oscarfiscalcp@gmail.com>
2025-04-11 12:53:33 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/carcenter/car_center_api/CarCenterApiApplication.java
@@ -0,0 +1,13 @@
+package com.carcenter.car_center_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCenterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCenterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carcenter/car_center_api/CarCenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carcenter.car_center_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
